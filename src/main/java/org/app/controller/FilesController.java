f6f1e03b3731bd7d0c398590685f23efbe5bb95d
StarLord1Art <ArtLep1542@yandex.ru>
2025-02-17 10:36:19 +0300

    Homework 2

--- src/main/java/org/app/controller/FilesController.java
+++ src/main/java/org/app/controller/FilesController.java
@@ -4,30 +4,27 @@ import org.app.controller.request.DirectoryInfoRequest;
 import org.app.service.FilesService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
-public class FilesController {
+public class FilesController implements FilesOperations {
   private final FilesService filesService;
 
   public FilesController(FilesService filesService) {
     this.filesService = filesService;
   }
 
-  @GetMapping("/files/info/{fileId}/{userId}")
-  public ResponseEntity<String> getFileInfo(@PathVariable("fileId") String fileId, @PathVariable("userId") String userId) {
+  @Override
+  public ResponseEntity<String> getFileInfo(String fileId, String userId) {
     return ResponseEntity.ok(filesService.getFileInfo(fileId, userId));
   }
 
-  @GetMapping("/directory/info")
-  public ResponseEntity<String> getFilesInDirectory(@RequestBody DirectoryInfoRequest directoryInfoRequest) {
+  @Override
+  public ResponseEntity<String> getFilesInDirectory(DirectoryInfoRequest directoryInfoRequest) {
     return ResponseEntity.ok(filesService.getFilesInDirectory(directoryInfoRequest));
   }
 
-  @GetMapping("/root/directories/{bucketName}")
-  public ResponseEntity<String> getRootDirectories(@PathVariable("bucketName") String bucketName) {
+  @Override
+  public ResponseEntity<String> getRootDirectories(String bucketName) {
     return ResponseEntity.ok(filesService.getRootDirectories(bucketName));
   }
 }
---
+++ src/main/java/org/app/controller/FilesOperations.java
@@ -0,0 +1,30 @@
+package org.app.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.app.controller.request.DirectoryInfoRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Files API", description = "Управление файлами")
+public interface FilesOperations {
+
+  @Operation(summary = "Получение информации о файле по ID")
+  @ApiResponse(responseCode = "200", description = "Информация о файле получена")
+  @GetMapping("/files/info/{fileId}/{userId}")
+  ResponseEntity<String> getFileInfo(@Parameter(description = "ID файла") @PathVariable("fileId") String fileId, @Parameter(description = "ID пользователя") @PathVariable("userId") String userId);
+
+  @Operation(summary = "Получение информации о файлах в папке по пути до папки и названию бакета")
+  @ApiResponse(responseCode = "200", description = "Информация о файлах в папке получена")
+  @GetMapping("/directory/info")
+  ResponseEntity<String> getFilesInDirectory(@Parameter(description = "Путь до папки и название бакета") @RequestBody DirectoryInfoRequest directoryInfoRequest);
+
+  @Operation(summary = "Получение корневых папок бакета по его названию")
+  @ApiResponse(responseCode = "200", description = "Корневые папки бакета получены")
+  @GetMapping("/root/directories/{bucketName}")
+  ResponseEntity<String> getRootDirectories(@Parameter(description = "Название бакета") @PathVariable("bucketName") String bucketName);
+}
